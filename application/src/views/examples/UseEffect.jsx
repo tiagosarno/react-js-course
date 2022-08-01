466a5d5fe1b5262aa2bbc8600d2cd13994f7086b
Tiago Sarno <tiago.sarno@cgadao.com.br>
2022-08-01 10:30:07 -0300

    feat(useEffect): exercício utilizando o Hook "useEffect"

--- application/src/views/examples/UseEffect.jsx
+++ application/src/views/examples/UseEffect.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+export default function UseEffect(props) {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    //FIXME: Efeito executado antes do componente ser renderizado
+    setInput("Setando valor antes da renderização do componente..");
+  }, []);
+
+  useEffect(() => {
+    console.log("Efeito após mudança no elemento controlado (input)");
+  }, [input]);
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <label>Input Text</label>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <h2>{input}</h2>
+      </div>
+    </div>
+  );
 }
-
-export default UseEffect

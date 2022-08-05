07d5ae45f9125adf728910be83472bb426e14dcb
Tiago Sarno <tiago.sarno@cgadao.com.br>
2022-08-05 10:00:52 -0300

    feat(useRef): exercício de fixação utilizando o Hook "useMemo"

--- application/src/views/examples/UseMemo.jsx
+++ application/src/views/examples/UseMemo.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useMemo, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseMemo = (props) => {
-    return (
-        <div className="UseMemo">
-            <PageTitle
-                title="Hook UseMemo"
-                subtitle="Retorna um valor memoizado!"
-            />
-        </div>
-    )
+function sum(a, b) {
+  const future = Date.now() + 2000;
+  while (Date.now() < future) {} // Espera de 2 segundos
+  return a + b;
 }
 
-export default UseMemo
+export default function UseMemo() {
+  const [n1, setN1] = useState(0);
+  const [n2, setN2] = useState(0);
+  const [n3, setN3] = useState(0);
+
+  const result = useMemo(() => {
+    return sum(n1, n2);
+  }, [n1, n2]);
+
+  return (
+    <div className="UseMemo">
+      <PageTitle title="Hook UseMemo" subtitle="Retorna um valor memoizado!" />
+      <SectionTitle title="Exercício useMemo()" />
+      <div className="center">
+        <input
+          type="number"
+          className="input"
+          value={n1}
+          onChange={(e) => setN1(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={n2}
+          onChange={(e) => setN2(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={n3}
+          onChange={(e) => setN3(parseInt(e.target.value))}
+        />
+        <span className="text">Soma: {result}</span>
+      </div>
+    </div>
+  );
+}

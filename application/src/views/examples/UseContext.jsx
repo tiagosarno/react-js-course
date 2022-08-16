46f0d8252f23ca9e9c23481c966e2fd997cee902
Tiago Sarno <tiago.sarno@cgadao.com.br>
2022-08-16 14:39:50 -0300

    feat(useContext): utilização do Hook useContext (Context API)

--- application/src/views/examples/UseContext.jsx
+++ application/src/views/examples/UseContext.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from "react";
+import { useContext } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+
+import DataContext from "../../data/DataContext";
 
 const UseContext = (props) => {
-    return (
-        <div className="UseContext">
-            <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
-        </div>
-    )
-}
+  const { state, setState } = useContext(DataContext);
 
-export default UseContext
+  function addNumber(delta) {
+    setState({
+      ...state,
+      number: state.number + delta,
+    });
+  }
+
+  return (
+    <div className="UseContext">
+      <PageTitle
+        title="Hook UseContext"
+        subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+      />
+      <div className="center">
+        <span className="text">{state.text}</span>
+      </div>
+      <div className="center">
+        <span className="text">{state.number}</span>
+      </div>
+      <dic className="center">
+        <button className="btn" onClick={() => addNumber(1)}>
+          +1
+        </button>
+        <button className="btn" onClick={() => addNumber(-1)}>
+          -1
+        </button>
+      </dic>
+    </div>
+  );
+};
+
+export default UseContext;
